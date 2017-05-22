a550ceac366cf1e3ac5850d05eb2b8887f4bfa18
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2017-05-21 23:13:36 -0700

    Reacomodar diccionario

---
+++ src/diccionario/numero.js
@@ -0,0 +1,27 @@
+/********
+ * Numero
+ * original: Number
+ ********/
+
+module.exports = {
+  // propiedades
+  AntiNumero: 'NaN',
+  ENTERO_SEGURO_MAXIMO: 'MAX_SAFE_INTEGER',
+  ENTERO_SEGURO_MINIMO: 'MIN_SAFE_INTEGER',
+  EPSILON: 'EPSILON',
+  INFINITO_NEGATIVO: 'NEGATIVE_INFINITY',
+  INFINITO_POSITIVO: 'POSITIVE_INFINITY',
+  VALOR_MAXIMO: 'MAX_VALUE',
+  VALOR_MINIMO: 'MIN_VALUE',
+
+  // metodos
+  aExponencial: 'toExponential',
+  aFraccionario: 'toFixed',
+  conPrecision: 'toPrecision',
+  deTextoAEntero: 'parseInt',
+  deTextoAFraccionario: 'parseFloat',
+  esAntiNumero: 'isNaN',
+  esEntero: 'isInteger',
+  esEnteroSeguro: 'isSafeInteger',
+  esFinite: 'isFinite',
+};
---
+++ src/diccionario/texto.js
@@ -0,0 +1,35 @@
+/********
+ * Texto
+ ********/
+
+module.exports = {
+  // metodos
+  aMayusculas: 'toUpperCase',
+  aMayusculasLocalizadas: 'toLocaleUpperCase',
+  aMinusculas: 'toLowerCase',
+  aMinusculasLocalizadas: 'toLocaleLowerCase',
+  ancla: 'anchor',
+  buscar: 'search',
+  characterEn: 'charAt',
+  codigoCaracterEn: 'charCodeAt',
+  coditoPuntoEn: 'codePointAt',
+  compararLocal: 'localeCompare',
+  copiarCaracteresPorCantidad: 'substr',
+  copiarCaracteresPorIndice: 'substring',
+  cortar: 'slice',
+  crudo: 'raw',
+  deCodigoCaracter: 'fromCharCode',
+  deCodigoPunto: 'fromCodePoint',
+  empiezaCon: 'startsWith',
+  encontrarExpresionRegular: 'match',
+  enlazar: 'link',
+  normalizar: 'normalize',
+  recortar: 'trim',
+  recortarDerecha: 'trimRight',
+  recortarIzquierda: 'trimLeft',
+  reemplazar: 'replace',
+  repetir: 'repeat',
+  separar: 'split',
+  terminaCon: 'endsWith',
+  valodDe: 'valueOf',
+};

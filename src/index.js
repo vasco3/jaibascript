f259ba4d5d6809681aa353f06aa54a86f905874e
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2017-07-09 20:42:56 -0700

    Remaping objects

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 
 const {
-  ObjetosEstandard,
   fundamentalObjects,
   globalFunctions,
   globalProperties,
-  PropiedadesMetodosDeObjetosEstandard
+  indexedCollections,
+  numbersAndDates,
+  PropiedadesMetodosDeObjetosEstandard,
+  textProcessing,
 } = require('./diccionario');
+
+const ObjetosEstandard = {};
 
 module.exports = function jaibascript({types: t}) {
   return {
     name: 'jaibascript',
     visitor: {
       CallExpression(path) {
-        const callee = ObjetosEstandard[path.node.callee.name] ||
-          globalFunctions[path.node.callee.name] || fundamentalObjects[path.node.callee.name];
+        const calleeName = path.node.callee.name;
+
+        const callee = numbersAndDates[calleeName] ||
+          indexedCollections[calleeName] ||
+          globalFunctions[calleeName] ||
+          fundamentalObjects[calleeName];
 
         if (callee) {
           path.node.callee = t.identifier(callee);
         }
       },
       ExpressionStatement(path) {
-        const expressionName = globalProperties[path.node.expression.name];
+        const nodeExpressionName = path.node.expression.name;
+        const expressionName = globalProperties[nodeExpressionName];
 
         if (expressionName) {
           path.node.expression = t.identifier(expressionName);
@@ -28,7 +37,8 @@ module.exports = function jaibascript({types: t}) {
       },
       MemberExpression(path) {
         const objectName = (path.node.object || {}).name || '';
-        const primitiveIdentifier = ObjetosEstandard[objectName];
+
+        const primitiveIdentifier = indexedCollections[objectName] || numbersAndDates[objectName] || textProcessing[objectName];
 
         if (primitiveIdentifier) {
           path.node.object = t.identifier(primitiveIdentifier);
@@ -41,7 +51,7 @@ module.exports = function jaibascript({types: t}) {
         }
       },
       NewExpression(path) {
-        const callee = ObjetosEstandard[path.node.callee.name];
+        const callee = numbersAndDates[path.node.callee.name];
 
         if (callee) {
           path.node.callee = t.identifier(callee);

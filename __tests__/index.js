93624a1c8d39565b79ac7c5bc7e9e82fd9f2af41
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2017-07-09 20:42:14 -0700

    Splitting tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,41 +1,25 @@
 const path = require('path');
-const pluginTester = require('babel-plugin-tester');
 const plugin = require('../src/index');
+const pluginTester = require('babel-plugin-tester');
+
+// Json blobs
+const arrayTests = require('./array');
+const booleanTests = require('./boolean');
+const dateTests = require('./date');
+const jsonTests = require('./json');
+const numberTests = require('./number');
+const stringTests = require('./string');
 
 pluginTester({
   plugin,
   fixtures: path.join(__dirname, '__fixtures__'),
   tests: [
-    {
-      title: 'Bool',
-      code: 'Booleano()',
-      output: 'Boolean()',
-    },
-    {
-      title: 'Fecha',
-      code: 'Fecha.prototipo.definirFecha()',
-      output: 'Date.prototype.setDate()',
-    },
-    {
-      title: 'nueva Fecha',
-      code: 'new Fecha()',
-      output: 'new Date()',
-    },
-    {
-      title: 'Lista',
-      code: 'Lista.prototipo.mapear()',
-      output: 'Array.prototype.map()',
-    },
-    {
-      title: 'mapear',
-      code: 'lista.mapear()',
-      output: 'lista.map()',
-    },
-    {
-      title: 'AntiNumero',
-      code: 'AntiNumero',
-      output: 'NaN',
-    },
+    ...arrayTests,
+    ...booleanTests,
+    ...dateTests,
+    ...jsonTests,
+    ...numberTests,
+    ...stringTests,
     {
       title: 'undefined',
       code: 'indefinido',
@@ -72,6 +56,11 @@ pluginTester({
       output: 'parseInt()',
     },
     {
+      title: 'AntiNumero',
+      code: 'AntiNumero',
+      output: 'NaN',
+    },
+    {
       title: 'isNaN',
       code: 'esAntiNumero()',
       output: 'isNaN()',
@@ -96,27 +85,5 @@ pluginTester({
       code: 'Infinito',
       output: 'Infinity',
     },
-    {
-      title: 'reducir',
-      code: 'ciudades.reducir()',
-      output: 'ciudades.reduce()',
-    },
-    {
-      title: 'Numero',
-      code: 'Numero.prototipo.esEntero()',
-      output: 'Number.prototype.isInteger()',
-    },
-    {
-      title: 'Texto',
-      code: 'Texto.prototipo.separar()',
-      output: 'String.prototype.split()',
-    },
-    {
-      title: 'JSON parse',
-      code: 'JSON.convertir()',
-      output: 'JSON.parse()',
-    },
   ],
 });
-
-
